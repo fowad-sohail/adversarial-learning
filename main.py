de8d52a4e755f060bad7230e95398dc2a6489480
Fowad Sohail <sohailf7@students.rowan.edu>
2020-10-03 12:09:07 -0400

    setup

---
+++ main.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+# PATH = 'path/to/saved/model'
+# torch.load()
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
