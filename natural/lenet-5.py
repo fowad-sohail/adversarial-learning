900a6ba9f875299b74f2d5fba117cacf8249cb60
Fowad Sohail <sohailf7@students.rowan.edu>
2020-11-17 13:28:24 -0500

    imports....

--- natural/lenet-5.py
+++ natural/lenet-5.py
@@ -6,32 +6,18 @@ import torchvision
 import torchvision.transforms as transforms
 import numpy as np
 
+# import sys
+# sys.path.append("..")
+
+from ..data_loader import load_cifar10
+
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 
 
-def load_cifar10():
-    transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    trainset = torchvision.datasets.CIFAR10(root='../data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                            shuffle=True, num_workers=0)
-
-    testset = torchvision.datasets.CIFAR10(root='../data', train=False,
-                                        download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                            shuffle=False, num_workers=0)
-
-    classes = ('plane', 'car', 'bird', 'cat',
-            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    
-    return trainloader, testloader
-
-train_loader, test_loader = load_cifar10()
+train_loader, test_loader = data_loader.load_cifar10()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
