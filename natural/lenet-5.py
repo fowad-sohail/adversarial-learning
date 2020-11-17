f536d140c01e5c959809b8a705cca390a74a32a4
Fowad Sohail <sohailf7@students.rowan.edu>
2020-11-17 14:12:25 -0500

    works?

---
+++ natural/lenet-5.py
@@ -0,0 +1,43 @@
+import torch
+
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+import sys
+sys.path.insert(1, './utils/')
+
+from load_and_test import load_cifar10, train_test
+
+
+train_loader, test_loader = load_cifar10()
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+print(device)
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = func.relu(self.conv1(x))
+        x = func.max_pool2d(x, 2)
+        x = func.relu(self.conv2(x))
+        x = func.max_pool2d(x, 2)
+        x = x.view(x.size(0), -1)
+        x = func.relu(self.fc1(x))
+        x = func.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+model = LeNet().to(device)
+
+
+train_test(model, device, train_loader, test_loader)
