f5658ec6cc5e00dcab6ecf8f13c459fc9ca65e9c
Fowad Sohail <sohailf7@students.rowan.edu>
2020-11-17 13:39:28 -0500

    import

---
+++ src/lenet-5.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+from ..utils.data_loader import load_cifar10
+
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+
+
+train_loader, test_loader = data_loader.load_cifar10()
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+print(device)
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = func.relu(self.conv1(x))
+        x = func.max_pool2d(x, 2)
+        x = func.relu(self.conv2(x))
+        x = func.max_pool2d(x, 2)
+        x = x.view(x.size(0), -1)
+        x = func.relu(self.fc1(x))
+        x = func.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+model = LeNet().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 100
+
+total_step = len(train_loader)
+loss_list = []
+acc_list = []
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # Run the forward pass
+        outputs = model(images.to(device))
+        loss = criterion(outputs, labels.to(device))
+        loss_list.append(loss.detach().cpu().numpy().item())
+
+        # Backprop and perform Adam optimisation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Track the accuracy
+        total = labels.size(0)
+        _, predicted = torch.max(outputs.detach().cpu(), 1)        
+        correct = (predicted == labels).sum().item()
+        acc_list.append(correct / total)
+
+        if (i + 1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
+                          (correct / total) * 100))
+
+print('Finished Training')
+train_acc = sum(acc_list) / len(acc_list)
+print('Training Accuracy: ' + str(train_acc))
+
+# Test the model
+val_loss_list = []
+val_acc_list = []
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        outputs = model(images.to(device))
+        loss = criterion(outputs, labels.to(device))
+        val_loss_list.append(loss.detach().cpu().numpy().item())
+        _, predicted = torch.max(outputs.data.detach().cpu(), 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        val_acc_list.append((correct / total) * 100)
+
+
+    print('Test Accuracy of the model on the 10000 test images: {} %'.format((correct / total) * 100))
+
