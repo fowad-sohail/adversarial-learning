7a64d144da699b962fe3a9f5841969d49f9bf021
Fowad Sohail <sohailf7@students.rowan.edu>
2020-11-17 14:13:52 -0500

    works?

--- utils/load_and_test.py
+++ utils/load_and_test.py
@@ -10,12 +10,12 @@ def load_cifar10():
         transform = transforms.Compose(
         [transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        trainset = torchvision.datasets.CIFAR10(root='/data/pytorch/', train=True,
+        trainset = torchvision.datasets.CIFAR10(root='/data/pytorch/CIFAR10', train=True,
                                                 download=True, transform=transform)
         trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                                 shuffle=True, num_workers=0)
 
-        testset = torchvision.datasets.CIFAR10(root='/data/pytorch/', train=False,
+        testset = torchvision.datasets.CIFAR10(root='/data/pytorch/CIFAR10', train=False,
                                                 download=True, transform=transform)
         testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                                 shuffle=False, num_workers=0)
