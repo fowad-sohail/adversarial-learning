fea053dc2693dd597ffce66ce3433fe95363d586
Fowad Sohail <sohailf7@students.rowan.edu>
2020-12-05 18:27:22 -0500

    ahhh

--- utils/load_and_test.py
+++ utils/load_and_test.py
@@ -112,14 +112,13 @@ def adversarial_test(model, device, test_loader, epsilon ):
 
         # Forward pass the data through the model
         output = model(data)
-        print(output)
-        print(output.max(1, keepdim=True))
         init_pred = output.argmax(1, keepdim=True)[1] # get the index of the max log-probability
         
         print()
         print(init_pred)
         print(target.argmax(0, keepdim=True)[1])
         print('---')
+        print('ITEMS')
         print(init_pred.cpu().item())
         print(target.argmax(0, keepdim=True)[1].cpu().item())
         # If the initial prediction is wrong, dont bother attacking, just move on
