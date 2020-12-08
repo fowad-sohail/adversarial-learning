9685802539e6035bd75180f00fb86a3de7f70326
Fowad Sohail <sohailf7@students.rowan.edu>
2020-12-08 14:10:58 -0500

    mlp

--- models/mlp.py
+++ models/mlp.py
@@ -21,7 +21,7 @@ print(device)
 class MLP(nn.Module):
     def __init__(self):
         super(MLP,self).__init__()
-        self.fc1 = nn.Linear(32*32, 512)
+        self.fc1 = nn.Linear(3*32*32, 512)
         self.fc2 = nn.Linear(512,512)
         self.fc3 = nn.Linear(512,256)
         self.fc4 = nn.Linear(256,10)
