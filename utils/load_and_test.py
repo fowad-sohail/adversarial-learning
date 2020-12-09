f61ed1281261e2baafd2519d67ff37adb00151d0
Fowad Sohail <sohailf7@students.rowan.edu>
2020-12-09 16:13:34 -0500

    cw atk

--- utils/load_and_test.py
+++ utils/load_and_test.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
+import cw
 
 def load_cifar10():
         transform = transforms.Compose(
@@ -157,6 +158,99 @@ def adversarial_test(model, device, test_loader, epsilon ):
     # Calculate final accuracy for this epsilon
     final_acc = correct/float(len(test_loader))*100
     print("Epsilon: {}\tTest Accuracy = {} / {} = {} %".format(epsilon, correct, len(test_loader), final_acc))
+
+    # Return the accuracy and an adversarial example
+    return final_acc, adv_examples
+
+
+def cw_attack(model, dataloader, mean, std):
+    inputs_box = (min((0 - m) / s for m, s in zip(mean, std)),
+                max((1 - m) / s for m, s in zip(mean, std)))
+    # an untargeted adversary
+    adversary = cw.L2Adversary(targeted=False,
+                            confidence=0.0,
+                            search_steps=10,
+                            box=inputs_box,
+                            optimizer_lr=5e-4)
+
+    inputs, targets = next(iter(dataloader))
+    adversarial_examples = adversary(model, inputs, targets, to_numpy=False)
+
+    # a targeted adversary
+    adversary = cw.L2Adversary(targeted=True,
+                            confidence=0.0,
+                            search_steps=10,
+                            box=inputs_box,
+                            optimizer_lr=5e-4)
+
+    inputs, _ = next(iter(dataloader))
+    # a batch of any attack targets
+    attack_targets = torch.ones(inputs.size(0)) * 3
+    adversarial_examples = adversary(net, inputs, attack_targets, to_numpy=False)
+
+
+def adversarial_test_cw(model, device, test_loader, epsilon ):
+    model.eval()
+    # Accuracy counter
+    correct = 0
+    adv_examples = []
+
+    # Loop over all examples in test set
+    for data, target in test_loader:
+
+        # Send the data and label to the device
+        data, target = data.to(device), target.to(device)
+
+        # Set requires_grad attribute of tensor. Important for Attack
+        data.requires_grad = True
+
+        # Forward pass the data through the model
+        output = model(data)
+        init_pred = output.argmax(1, keepdim=True)[1] # get the index of the max log-probability
+        
+
+        fixed_init_pred = init_pred.cpu()
+        fixed_target = target.argmax(0, keepdim=True).cpu()
+
+        # If the initial prediction is wrong, dont bother attacking, just move on
+        if fixed_init_pred.item() != fixed_target.item():
+            continue
+
+        # Calculate the loss
+        loss = F.nll_loss(output, target)
+
+        # Zero all existing gradients
+        model.zero_grad()
+
+        # Calculate gradients of model in backward pass
+        loss.backward()
+
+        # Collect datagrad
+        data_grad = data.grad.data
+
+        # Call CW Attack
+        perturbed_data = cw_attack(model, dataloader, mean, std)
+
+        # Re-classify the perturbed image
+        output = model(perturbed_data)
+
+        # Check for success
+        final_pred = output.argmax(1, keepdim=True)[1] # get the index of the max log-probability
+        if final_pred.cpu().item() == fixed_target.item():
+            correct += 1
+            # Special case for saving 0 epsilon examples
+            if (epsilon == 0) and (len(adv_examples) < 5):
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (fixed_init_pred.item(), final_pred.cpu().item(), adv_ex) )
+        else:
+            # Save some adv examples for visualization later
+            if len(adv_examples) < 5:
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (fixed_init_pred.item(), final_pred.cpu().item(), adv_ex) )
+
+    # Calculate final accuracy for this epsilon
+    final_acc = correct/float(len(test_loader))*100
+    print(final_acc)
 
     # Return the accuracy and an adversarial example
     return final_acc, adv_examples
